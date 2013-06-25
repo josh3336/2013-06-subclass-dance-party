d316f23277f9c852915124a3b4cd997f47b73f7d
Hack Reactor Students <github@hackreactor.com>
2013-06-24 18:13:32 -0700

    successfully switched to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,16 +1,13 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
-  debugger;
+  this.timeBetweenSteps=timeBetweenSteps;
   this.$node = $('<span class="dancer"></span>');
-  this.step();
   this.setPosition(top, left);
-
+  this.step();
 };
 
-
-makeDancer.prototype = {
-  setPosition : function(top, left){
+makeDancer.prototype.setPosition = function(top, left){
     /* Use css top and left properties to position our <span> tag
      * where it belongs on the page. See http://api.jquery.com/css/
      */
@@ -19,11 +16,12 @@ makeDancer.prototype = {
       left: left
     };
     this.$node.css(styleSettings);
-  },
+  };
 
-   step : function(){
+makeDancer.prototype.step = function(){
+
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(this.step, timeBetweenSteps);
-  }
+    var that = this;
+    setTimeout(function(){that.step();}, this.timeBetweenSteps);
 };
